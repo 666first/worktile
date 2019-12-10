ad59f04e47a3aab3e08062d4567fe45cd9da5118
MxLsSb <you@example.com1069236447@qq.com>
2019-12-10 13:46:08 +0800

    first commitsrc

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,77 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
+      <router-link to="/item">item</router-link>
+      <router-link to="/calendar">calendar</router-link>
+      <router-link to="/meshdisk">meshdisk</router-link>
+      <router-link to="/briefing">briefing</router-link>
     </div>
-    <router-view/>
+    <router-view />
+    <!-- <el-col :span="2">
+      <div class="nav">
+        <el-menu :default-active="activeIndex" background-color="#22d7bb">
+          <el-menu-item index="1">
+            <router-link to="/">Home</router-link>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <router-link to="/item">item</router-link>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <router-link to="/calendar">calendar</router-link>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <router-link to="/meshdisk">meshdisk</router-link>
+          </el-menu-item>
+          <el-menu-item index="5">
+            <router-link to="/briefing">briefing</router-link>
+          </el-menu-item>
+        </el-menu>
+      </div>
+    </el-col>
+    <el-col :span="22">
+        <router-view />
+    </el-col> -->
   </div>
 </template>
-
-<style lang="scss">
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: "1"
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+  height: 100%;
 }
-
+// .nav{
+//   height: 100%;
+// }
 #nav {
-  padding: 30px;
-
+  position: fixed;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  background: #42b983;
+  width: 80px;
   a {
+    display: block;
     font-weight: bold;
     color: #2c3e50;
-
+    height: 80px;
+    line-height: 80px;
     &.router-link-exact-active {
-      color: #42b983;
+      color: #fff;
+      background: #2c3e50;
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import News from '../views/news'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'news',
+    component: News
   },
   {
     path: '/about',
@@ -17,6 +17,26 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/item',
+    name: 'item',
+    component: () => import(/* webpackChunkName: "item" */ '../views/item')
+  },
+  {
+    path: '/calendar',
+    name: 'calendar',
+    component: () => import(/* webpackChunkName: "calendar" */ '../views/calendar')
+  },
+  {
+    path: '/meshdisk',
+    name: 'meshdisk',
+    component: () => import(/* webpackChunkName: "meshdisk" */ '../views/meshdisk')
+  },
+  {
+    path: '/briefing',
+    name: 'briefing',
+    component: () => import(/* webpackChunkName: "briefing" */ '../views/briefing')
   }
 ]
 
